f9872ceb5a5a70a9d25ec4b4fe2770e609284b9b
Nick Salloum <nick@callmenick.com>
2015-09-29 14:10:54 -0700

    (init)

---
+++ README.md
@@ -0,0 +1,13 @@
+# Five Star Rating
+
+A five star rating template.
+
+## Live Demo
+
+View the live demo here.
+
+## License & Copyright
+
+Licensed under the [MIT license.](http://www.opensource.org/licenses/mit-license.php)
+
+Copyright 2014, [Call Me Nick.](http://callmenick.com)
---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+(function() {
+
+  'use strict'
+
+  /**
+   * Requirements
+   */
+  var gulp = require('gulp');
+  var autoprefixer = require('gulp-autoprefixer');
+  var cssmin = require('gulp-cssmin');
+  var concat = require('gulp-concat');
+  var jshint = require('gulp-jshint');
+  var notify = require("gulp-notify");
+  var rename = require('gulp-rename');
+  var sass = require('gulp-sass');
+  var stylish = require('jshint-stylish');
+  var uglify = require('gulp-uglify');
+
+  /**
+   * Paths
+   */
+  var paths = {
+    sass: ['./sass/**/*.scss'],
+    scripts: ['./js/src/**/*.js']
+  };
+
+  /**
+   * Styles
+   */
+  gulp.task('styles', function() {
+    return gulp.src(paths.sass)
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }))
+      .on('error', notify.onError({
+        title: 'Error compiling Sass',
+        message: 'Check the console for info'
+      }))
+      .on('error', sass.logError)
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./css'))
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css'));
+  });
+
+  /**
+   * Styles watcher
+   */
+  gulp.task('stylesDev', function() {
+    gulp.watch(paths.sass, ['styles']);
+  });
+
+  /**
+   * Scripts linting
+   */
+  gulp.task('lint', function() {
+    return gulp.src(paths.scripts)
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
+      .pipe(jshint.reporter('fail'))
+      .on('error', notify.onError({
+        title: 'JS hint failed',
+        message: 'Check the console for errors'
+      }));
+  });
+
+  /**
+   * Scripts concat
+   */
+  gulp.task('concat', function() {
+    return gulp.src(paths.scripts)
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('./js/dist/'))
+      .pipe(uglify())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('./js/dist/'));
+  });
+
+  /**
+   * Scripts dev
+   */
+  gulp.task('scriptsDev', function() {
+    gulp.watch(paths.scripts, ['lint']);
+  });
+
+  /**
+   * Final tasks - these are the tasks that should be run from the command line,
+   * as they encompass the above.
+   */
+  gulp.task('default', ['styles', 'lint', 'stylesDev', 'scriptsDev']);
+  gulp.task('styles:dev', ['styles', 'stylesDev']);
+  gulp.task('scripts:dev', ['lint', 'scriptsDev']);
+  gulp.task('scripts:build', ['lint', 'concat']);
+
+})();
